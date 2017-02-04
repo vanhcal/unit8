d759dfb9402da96415b311277b617647745cc647
vanhcal <mellychen87@gmail.com>
2017-02-03 16:02:02 -0800

    Add files via upload

--- project.py
+++ project.py
@@ -99,7 +99,7 @@ def gconnect():
   stored_gplus_id = login_session.get('gplus_id')
   if stored_credentials is not None and gplus_id == stored_gplus_id:
     response = make_response(json.dumps('Current user is already connected.'), 200)
-    response.headers['Content-Type' = 'application/json']
+    response.headers['Content-Type'] = 'application/json'
     return response
 
   # if none of these if statements are true, we have a valid access token, and user is able to successfully log into server. store the access token in the session for later use 
